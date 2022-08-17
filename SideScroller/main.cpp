0fb4969df5f193ffd2d6854b1b6dcfe82f25fadc
Leon Lowe <leonlowe0@outlook.com>
2022-08-17 13:48:37 +0100

    Comments
    Added the following:
     - Helpful comments for viewing purposes

--- SideScroller/main.cpp
+++ SideScroller/main.cpp
@@ -1,16 +1,26 @@
+/*
+    * main.cpp
+    * A game where you must jump over obstacles
+    * in order to get a new high score
+    * Made by Leon Lowe
+*/
+
 #include "raylib.h"
 
 int main()
 {
-    const int SCREEN_WIDTH = 1280;
-    const int SCREEN_HEIGHT = 720;
+    //Window variabes
+    const int WINDOW_WIDTH = 1280;
+    const int WINDOW_HEIGHT = 720;
     const char* WINDOW_NAME = "Dapper Dasher";
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME);
+    //Initialise window and set traget fps
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
     SetTargetFPS(60);
 
     bool running = true;
 
+    //Main game loop
     while(running)
     {
         if(WindowShouldClose())
@@ -24,6 +34,9 @@ int main()
 
         EndDrawing();
     }
+
+    //Close the game
+    CloseWindow();
 
     return 0;
 }
