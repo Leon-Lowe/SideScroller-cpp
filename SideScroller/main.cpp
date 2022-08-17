552bfff4e1bbc3e34541cc74e22a206e717603d7
Leon Lowe <leonlowe0@outlook.com>
2022-08-17 14:27:36 +0100

    Jumping
    Added the following:
     - Jumping
     - Gravity
     - Ground check

--- SideScroller/main.cpp
+++ SideScroller/main.cpp
@@ -18,6 +18,16 @@ int main()
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
     SetTargetFPS(60);
 
+    const int GRAVITY = 1;
+
+    //Rectangle variables
+    const int RECT_WIDTH = 50;
+    const int RECT_HEIGHT = 80;
+    int rectPosY = WINDOW_HEIGHT - RECT_HEIGHT;
+    int rectVelocity = 0;
+    const int RECT_JUMP_FORCE = 22;
+    bool isInAir = false;
+
     bool running = true;
 
     //Main game loop
@@ -31,6 +41,31 @@ int main()
         BeginDrawing();
 
         ClearBackground(WHITE);
+
+        //Ground check
+        if(rectPosY >= WINDOW_HEIGHT - RECT_HEIGHT)
+        {
+            rectVelocity = 0;
+            rectPosY = WINDOW_HEIGHT - RECT_HEIGHT;
+            isInAir = false;
+        }
+        else
+        {
+            //Apply gravity
+            rectVelocity += GRAVITY;
+            isInAir = true;
+        }
+
+        //Jump check
+        if(IsKeyPressed(KEY_SPACE) && !isInAir)
+        {
+            rectVelocity -= RECT_JUMP_FORCE;
+        }
+
+        //Update position
+        rectPosY += rectVelocity;
+
+        DrawRectangle((WINDOW_WIDTH/2) - (RECT_WIDTH/2), rectPosY, RECT_WIDTH, RECT_HEIGHT, BLUE);
 
         EndDrawing();
     }
